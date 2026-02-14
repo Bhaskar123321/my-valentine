6e18b2c235dd7b5e9d14d8f4d7a1aee8624883c3
Bhaskar Kumar Ambasta <bhaskarambast28@gmail.com>
2026-02-14 13:21:29 +0530

    Initial commit: Valentine's Day project

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { useState } from "react";
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "What if I asked really nicely?",
+      "No! please",
+      "With a chocolate cake?",
+      "What about a good shopping trip?",
+      "PLEASE Shona Ji",
+      "But :*(",
+      "If we go on a trip?",
+      "Nice date?",
+      "Still No...Humm",
+      "Dekhlo naa ka matlb haan bhi bol sakti ho :P",
+      ":((((",
+      "PRETTY PLEASE",
+      "Estoy muerto",
+      "No :(",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="-mt-16 flex h-screen flex-col items-center justify-center">
+      {yesPressed ? (
+        <>
+          <img src="https://gifdb.com/images/high/love-you-forever-together-forever-gif-kpstcfylcthi8czc.gif" />
+          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you Shugu!! ;))</div>
+        </>
+      ) : (
+        <>
+          <img
+            className="h-[200px]"
+            src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+          />
+          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <div className="flex items-center">
+            <button
+              className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
